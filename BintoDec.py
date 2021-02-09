80b606a6806c0d8ccfc710b1380e279c2583829c
Jamie-whitehair <78787172+Jamie-whitehair@users.noreply.github.com>
2021-02-09 17:09:28 +0000

    Add files via upload

---
+++ BintoDec.py
@@ -0,0 +1,24 @@
+import math
+loop0 = True
+while loop0 is True:
+    dec = int(input("type the number you want to convert to Binary. \n"))
+    if dec > 255:
+        print("value cant exceed 255")
+
+    else:
+        loop0 = False
+# loop0 is for numbers input
+# loop1 is for the binary loop
+
+loop1 = True
+while loop1 is True:
+    if dec == 0:
+        loop1 = False
+    else: 
+        dec1 = dec // 2
+        dec3 = dec % 2
+        dec = dec1
+        print(dec3)
+        # simple loop to solve problem
+
+input()
